5b42efaa4700d2a32dcf3a49f311d39ee36dbf5a
Aryajeet Ghosh <aryajeetghosh1@gmail.com>
2023-10-02 02:52:42 +0530

    chore: header, footer updated

--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -11,10 +11,10 @@ const Header = () => {
                     <span className="ml-3 text-xl">Tailblocks</span>
                 </a>
                 <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                    <a className="mr-5 hover:text-gray-900">First Link</a>
-                    <a className="mr-5 hover:text-gray-900">Second Link</a>
-                    <a className="mr-5 hover:text-gray-900">Third Link</a>
-                    <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+                    <a className="mr-5 hover:text-gray-900 cursor-pointer">Home</a>
+                    <a className="mr-5 hover:text-gray-900 cursor-pointer">About Me</a>
+                    <a className="mr-5 hover:text-gray-900 cursor-pointer">Blog</a>
+                    <a className="mr-5 hover:text-gray-900 cursor-pointer">Settings</a>
                 </nav>
                 <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Get Started
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
