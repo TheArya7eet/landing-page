a1b1826d05d1dc4a31e874ff327b6e86fd002fb9
Aryajeet Ghosh <aryajeetghosh1@gmail.com>
2023-10-02 03:51:31 +0530

    feat: feature added

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 
 import HeroSection from '@/components/hero-section'
 import Content from '@/components/content'
+import Feature from '@/components/feature'
 
 export default function Home() {
   return (
     <main>
         <HeroSection />
         <Content />
+        <Feature />
     </main>
   )
 }
--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -12,8 +12,8 @@ const Header = () => {
                 </a>
                 <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
                     <a className="mr-5 hover:text-gray-900 cursor-pointer">Home</a>
-                    <a className="mr-5 hover:text-gray-900 cursor-pointer">About Me</a>
-                    <a className="mr-5 hover:text-gray-900 cursor-pointer">Blog</a>
+                    <a className="mr-5 hover:text-gray-900 cursor-pointer">Content</a>
+                    <a className="mr-5 hover:text-gray-900 cursor-pointer">Features</a>
                     <a className="mr-5 hover:text-gray-900 cursor-pointer">Settings</a>
                 </nav>
                 <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Get Started
