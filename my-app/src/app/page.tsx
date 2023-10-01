81a5e80302a84dcdc753718bb9d172b9049af74f
Aryajeet Ghosh <aryajeetghosh1@gmail.com>
2023-10-02 03:39:31 +0530

    feat: content added

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 
 import HeroSection from '@/components/hero-section'
+import Content from '@/components/content'
 
 export default function Home() {
   return (
     <main>
         <HeroSection />
+        <Content />
     </main>
   )
 }
