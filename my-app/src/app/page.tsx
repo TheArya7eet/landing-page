23cdd30f22f2adbaa9aa5ca3a9023b18bc44e8c0
Aryajeet Ghosh <aryajeetghosh1@gmail.com>
2023-10-02 03:09:46 +0530

    feat: hero-section added

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 
+import HeroSection from '@/components/hero-section'
+
 export default function Home() {
   return (
     <main>
-        {/* <h1>Hello World</h1> */}
+        <HeroSection />
     </main>
   )
 }
