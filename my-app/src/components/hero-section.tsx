01fe1692f80642e6e72ba0eacc8c7a1d3d9bf2c5
Aryajeet Ghosh <aryajeetghosh1@gmail.com>
2023-12-23 00:18:29 +0530

    chore: header, hero-section, footer updated

--- my-app/src/components/hero-section.tsx
+++ my-app/src/components/hero-section.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import Image from 'next/image';
+import profilePic from '../public/pfp.jpg';
 
 const HeroSection = () => {
     return (
         <section className="text-gray-400  body-font">
             <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col lg:flex-row">
                 <div className="group relative rounded-3xl shadow-xl  space-y-6 overflow-hidden">
-                    <img
-                        className="mx-auto h-[26rem] w-full grayscale object-cover object-top transition duration-500 group-hover:scale-105 group-hover:grayscale-0 text-center"
-                        src=""
-                        loading="lazy"
-                        width="640"
-                        height="805"
-                    />
-                    <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-indigo-500 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
-                        <div>
-                            <h4 className="text-xl font-semibold dark:text-gray-700 text-white">Aryajeet Ghosh</h4>
-                            <span className="block text-sm text-gray-300">Frontend Developer  and Programmer</span>
+                    <div className="group relative rounded-3xl shadow-xl  space-y-6 overflow-hidden">
+                        <img
+                            className="mx-auto h-[26rem] w-full grayscale object-cover object-top transition duration-500 group-hover:scale-105 group-hover:grayscale-0 text-center"
+                            src=""
+                            loading="lazy"
+                            width="640"
+                            height="805"
+                        />
+                        <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-indigo-500 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
+                            <div>
+                                <h4 className="text-xl font-semibold dark:text-gray-700 text-white">Aryajeet Ghosh</h4>
+                                <span className="block text-sm text-gray-300">Frontend Developer  and Programmer</span>
+                            </div>
+                            <p className="mt-8 text-gray-300 dark:text-gray-600">I am passionate about web design and development. I produce some quality work.</p>
                         </div>
-                        <p className="mt-8 text-gray-300 dark:text-gray-600">I am passionate about web design and development. I produce some quality work.</p>
                     </div>
                 </div>
                 <div className="text-center mt-5 lg:w-2/3 w-full">
